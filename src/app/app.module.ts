e3ddeb843fac56733747568e60a024c06370e5cc
Blake Martin <BMartin@Rediker.com>
2018-05-18 15:11:10 -0400

    initial working version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,42 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgxErrorsModule } from '@ultimate/ngxerrors';
+import { AngularFireModule } from 'angularfire2';
+import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { firebaseConfig } from '../config';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    SignupPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig.fire),
+    NgxErrorsModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    SignupPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService,
+    AngularFireAuth,
   ]
 })
 export class AppModule {}
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  fire: {
+    apiKey: "AIzaSyCVu071iYiC7LeswcaEv32Og8bnk4I-1jI",
+    authDomain: "groupnotificationsystem.firebaseapp.com",
+    databaseURL: "https://groupnotificationsystem.firebaseio.com",
+    projectId: "groupnotificationsystem",
+    storageBucket: "groupnotificationsystem.appspot.com",
+    messagingSenderId: "839215161924"
+  }
+};
